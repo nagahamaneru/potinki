70e607f01de026efff2ed87a8dbbd1bf9fa44b64
Nrita joto <j.narita.sys20@morijyobi.ac.jp>
2021-03-30 12:05:40 +0900

    パーペキです

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ def top():
 
 @app.route("/kyuuyo")
 def kyuuyo():
-    return render_template("kyuuyo.html")
+    kyuuyo = request.args.get("kyuuyo")
+    return render_template("kyuuyo.html", kyuuyo=kyuuyo)
+
+@app.route("/kyuuyoanswer")
+def kyuuyoanswer():
+    return render_template("kyuuyoanswer.html")
+
 
 @app.route("/enn")
 def enn():
     en = request.args.get("en")
     return render_template("enn.html", en=en)
+
+@app.route("/ennanswer")
+def ennanswer():
+    return render_template("ennanswer.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
